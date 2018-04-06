ce9dddff896229384e2746b1a2476472fa1305e3
nkand84 <nityabhagi@gmail.com>
2018-04-06 15:34:55 -0700

    empty search on movie-this fixed

--- liri.js
+++ liri.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 
+var keys = require('./keys.js');
+
+var Spotify = require('node-spotify-api');
+console.log('====== keys ======')
+console.log(keys.spotify.id)
+console.log('====== end of keys ======')
+//request
+var request = require("request");
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+
+
 // user input
 var action = process.argv[2];
 var value = process.argv[3];
 var movieName = "";
 var songName = "";
-//request
-var request = require("request");
+
 // switch case 
 switch (action) {
     case "my-tweets":
@@ -14,7 +27,7 @@ switch (action) {
         break;
 
     case "spotify-this-song":
-        spotifyThisSong();
+        spotifyThisSong(value);
         break;
 
     case "movie-this":
@@ -27,21 +40,9 @@ switch (action) {
 }
 // OMDB request
 function movieThis() {
-    if (value === ''){
-       
-        request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(JSON.parse(body));
-            }
-        });
-            console.log("Title of the Movie: " + JSON.parse(body).Title);
-            console.log("Year the movie came out: " + JSON.parse(body).Year);
-            console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot of the movie: " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    if (typeof value === 'undefined') {
+
+        request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy", omdbResponse)
 
     }
     else {
@@ -49,32 +50,39 @@ function movieThis() {
         //     movieName = movieName + process.argv[i] + " ";
         // }
         movieName = value;
-        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                // console.log(JSON.parse(body));
-            console.log("Title of the Movie: " + JSON.parse(body).Title);
-            console.log("Year the movie came out: " + JSON.parse(body).Year);
-            console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot of the movie: " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
-            }
+        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", omdbResponse)
 
-        });
     }
-    
+
 }
+function omdbResponse(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        // console.log(JSON.parse(body));
+        console.log("Title of the Movie: " + JSON.parse(body).Title);
+        console.log("Year the movie came out: " + JSON.parse(body).Year);
+        console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+        console.log("Language of the movie: " + JSON.parse(body).Language);
+        console.log("Plot of the movie: " + JSON.parse(body).Plot);
+        console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    }
+
+};
+
+
+
+
 // spotify request
-function  spotifyThisSong(){
-    if(value == ""){
+function spotifyThisSong(value) {
+    console.log(value);
+    spotify.search({ type: "track", query: value}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    for()
+        console.log(JSON.stringify(data.tracks.items[0], null, 2));
+    });
 
-    }
-    else {
-        songName = value;
-        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-            if (!error && response.statusCode === 200) {
 
-    }
 }
