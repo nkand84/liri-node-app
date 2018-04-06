e4a3119ebdb03d7bbc52731d3c085f03333c0751
nkand84 <nityabhagi@gmail.com>
2018-04-05 23:34:01 -0700

    got movie function working except for empty search

--- liri.js
+++ liri.js
@@ -1,1 +1,67 @@
 require("dotenv").config();
+
+// user input
+var action = process.argv[2];
+var value = process.argv[3];
+var movieName = "";
+//request
+var request = require("request");
+// switch case 
+switch (action) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
+
+function movieThis() {
+    if (value === ""){
+        movieName = "Mr.Nobody";
+        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(JSON.parse(body));
+            }
+        });
+            console.log("Title of the Movie: " + JSON.parse(body).Title);
+            console.log("Year the movie came out: " + JSON.parse(body).Year);
+            console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+            console.log("Language of the movie: " + JSON.parse(body).Language);
+            console.log("Plot of the movie: " + JSON.parse(body).Plot);
+            console.log("Actors in the movie: " + JSON.parse(body).Actors);
+
+    }
+    else {
+        for (var i = 3; i < process.argv.length; i++) {
+            movieName = movieName + process.argv[i] + " ";
+        }
+        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(JSON.parse(body));
+            }
+            console.log("Title of the Movie: " + JSON.parse(body).Title);
+            console.log("Year the movie came out: " + JSON.parse(body).Year);
+            console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+            console.log("Language of the movie: " + JSON.parse(body).Language);
+            console.log("Plot of the movie: " + JSON.parse(body).Plot);
+            console.log("Actors in the movie: " + JSON.parse(body).Actors);
+
+
+        });
+    }
+    
+}
