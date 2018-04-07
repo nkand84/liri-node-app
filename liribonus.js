7925887460fa73f1f325a179005b9b3774e57299
nkand84 <nityabhagi@gmail.com>
2018-04-07 16:30:21 -0700

    adding liri bonus- writing log data into data.txt, successgit add .

--- liribonus.js
+++ liribonus.js
@@ -43,43 +43,31 @@ switch (action) {
 // OMDB request
 function movieThis(movieName) {
     if (typeof movieName === 'undefined') {
-
         request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy", omdbResponse)
-
     }
     else {
         request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", omdbResponse)
-
     }
 
 }
 // OMDB response 
 function omdbResponse(error, response, body) {
-    var fs = require("fs");
-    // write the command line data into a text file
-    fs.appendFile("data.txt", "\nTitle of the Movie: " + JSON.parse(body).Title + "\nYear the movie came out: " + JSON.parse(body).Year + "\nIMDB Rating of the movie: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value + "\nCountry where the movie was produced: " + JSON.parse(body).Country + "\nLanguage of the movie: " + JSON.parse(body).Language + "\nPlot of the movie: " + JSON.parse(body).Plot +
-        "\nActors in the movie: " + JSON.parse(body).Actors+"\n====================================================", function (err) {
-            // If the code experiences any errors it will log the error to the console.
-            if (err) {
-                return console.log(err);
-            }
-            // Otherwise, it will print: "movies.txt was updated!"
-            console.log("cmd-data.txt was updated!");
-        });
-    if (!error && response.statusCode === 200) {
-        // console.log(JSON.parse(body));
-        console.log("---------------------------------------------------------------------------------------");
-        console.log("Title of the Movie: " + JSON.parse(body).Title);
-        console.log("Year the movie came out: " + JSON.parse(body).Year);
-        console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-        console.log("Language of the movie: " + JSON.parse(body).Language);
-        console.log("Plot of the movie: " + JSON.parse(body).Plot);
-        console.log("Actors in the movie: " + JSON.parse(body).Actors);
+       if (!error && response.statusCode === 200) {
+        var movieToString = "";
+        movieToString += "\n=========================================================================";
+        movieToString += "\nTitle of the Movie: " + JSON.parse(body).Title;
+        movieToString += "\nYear the movie came out: " + JSON.parse(body).Year;
+        movieToString += "\nIMDB Rating of the movie: " + JSON.parse(body).imdbRating;
+        movieToString += "\nRotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value;
+        movieToString += "\nCountry where the movie was produced: " + JSON.parse(body).Country;
+        movieToString += "\nLanguage of the movie: " + JSON.parse(body).Language;
+        movieToString += "\nPlot of the movie: " + JSON.parse(body).Plot;
+        movieToString += "Actors in the movie: " + JSON.parse(body).Actors;
+        console.log("Movie Info => "+ movieToString);
+        var fs = require("fs");
+        // write the command line data into a text file
+        fs.appendFile("data.txt",movieToString, appendData)
     }
-
-
 };
 
 // spotify request
@@ -99,35 +87,20 @@ function spotifyResponse(err, data) {
         return console.log('Error occurred: ' + err);
     }
     var spotifyObj = data.tracks.items;
-    var spotifyToString = ""
+    var spotifyToString = "";
     // looping thru array of items inside the object
     for (var j = 0; j < spotifyObj.length; j++) {
-        spotifyToString += "\n--------------------------------------------------------------";
+        spotifyToString += "\n--------------------------Spotify Data-----------------------------------";
         spotifyToString += "\nSong Name => " + spotifyObj[j].name;
         spotifyToString += "\nSpotify Preview Link => " + spotifyObj[j].preview_url;
         spotifyToString += "\nAlbum the song is from => " + spotifyObj[j].album.name;
-        
-        //console.log("--------------------------------------------------------------------------------");
-        //console.log("Song Name => " + spotifyObj[j].name);
-        //console.log("Spotify Preview Link => " + spotifyObj[j].preview_url);
-        //console.log("Album the song is from => " + spotifyObj[j].album.name);
         for (var i = 0; i < spotifyObj[j].artists.length; i++) {
             spotifyToString += "\nArtists => " + spotifyObj[j].artists[i].name
-            //console.log("Artists => " + spotifyObj[j].artists[i].name);
         }
     }
     console.log("==================================================");
     console.log("This string goes into the data.txt file" + spotifyToString);
-    fs.appendFile("data.txt",spotifyToString,function(err){
-        if (err) {
-            console.log(err);
-          }
-        
-          // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-          else {
-            console.log("Spotify Data Added!");
-          }
-    });
+    fs.appendFile("data.txt", spotifyToString,appendData)
 }
 
 
@@ -136,19 +109,31 @@ function myTweets() {
     var params = { screen_name: 'knkool84' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
+            var tweetsToString = ""
             // looping through an array of tweet info
             for (var t = 0; t < tweets.length; t++) {
-                console.log("---------------------------------------------------------------");
-                // logging the date or time stamp of the tweet
-                console.log(tweets[t].created_at);
-                // text of the tweet
-                console.log(tweets[t].text);
+                tweetsToString += "\n--------------------------Twitter Data--------------------------------";
+                tweetsToString += "\n" + tweets[t].created_at;
+                tweetsToString += "\n" + tweets[t].text;
                 if (t === 4) { break; }
             }
+            console.log(tweetsToString);
+            var fs = require("fs");
+            fs.appendFile("data.txt", tweetsToString, appendData)
         }
     });
 }
+// append function
+function appendData(err) {
+    if (err) {
+        console.log(err);
+    }
 
+    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+    else {
+        console.log("Data file updated!");
+    }
+}
 function doWhatItSays() {
     var fs = require("fs");
     // read file random.txt
